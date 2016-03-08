1cd05f1dc9a7a81c8b83aca3ef8fb920330a053e
GrandMasterZ <zambacevicius.mantas@gmail.com>
2016-03-08 22:52:00 +0200

    View
    Sutvarkytas footer ir idetas view su restorano funkcionalumu

--- src/ImkStala/wwwroot/js/site.js
+++ src/ImkStala/wwwroot/js/site.js
@@ -5,3 +5,30 @@
 $(function () {
     $("#datepicker").datepicker();
 });
+
+function addToType(id) {
+    var obj = document.getElementById(id);
+    var val = obj.value;
+    val++;
+    obj.value = val;
+
+}
+function downToType(id) {
+    var obj = document.getElementById(id);
+    var val = obj.value;
+    if (val > 0) {
+        val--;
+        obj.value = val;
+    }
+}
+function keyValidation(evt) {
+    var key = window.evt ? evt.keyCode : evt.which;
+    if (evt.keyCode == 8 || evt.keyCode == 46
+         || evt.keyCode == 37 || evt.keyCode == 39) {
+        return true;
+    }
+    else if (key < 48 || key > 57) {
+        return false;
+    }
+    else return true;
+}
