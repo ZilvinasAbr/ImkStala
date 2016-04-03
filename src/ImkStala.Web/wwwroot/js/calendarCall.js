099e6a47e30fec7de8f4ed8a6ace128f016bae60
GrandMasterZ <zambacevicius.mantas@gmail.com>
2016-04-03 13:45:45 +0300

    Added timepicker and new hidden field which holds restaurant id

---
+++ src/ImkStala.Web/wwwroot/js/calendarCall.js
@@ -0,0 +1,9 @@
+﻿var picker = new Pikaday({
+    field: document.getElementById('datepicker'),
+    bound: false,
+    format: 'YYYY-MM-DD',
+    container: document.getElementById('container')
+});
+
+var $input = $('#timepicker').pickatime({
+})
