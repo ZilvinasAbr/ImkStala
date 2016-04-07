82c20e6eefad7ecba9131f562bf6e6453d85f2be
Ignas Savickas <ignuxs34@gmail.com>
2016-04-07 14:30:17 +0300

    Easier password

--- src/ImkStala.Web/Startup.cs
+++ src/ImkStala.Web/Startup.cs
@@ -59,7 +59,14 @@ namespace ImkStala.Web
                 .AddDbContext<ApplicationDbContext>(options =>
                     options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
+            services.AddIdentity<ApplicationUser, IdentityRole>(o =>
+            {
+                o.Password.RequireDigit = false;
+                o.Password.RequireLowercase = false;
+                o.Password.RequireUppercase = false;
+                o.Password.RequireNonLetterOrDigit = false;
+                o.Password.RequiredLength = 6;
+            })
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
